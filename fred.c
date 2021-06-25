a3970dc3b891dfee4c1f8c41a5f785adf56e8f3f
mulifred <85351893+mulifred@users.noreply.github.com>
2021-06-25 13:56:51 +0300

    Add files via upload

---
+++ fred.c
@@ -0,0 +1,11 @@
+//c program showing usage of assignment operators
+#include <stdio.h>
+int main()
+{
+int a;
+printf("enter a");
+scanf("%d",&a);
+a-=10;
+printf("value of a is %d",a);
+return 0;	
+}
