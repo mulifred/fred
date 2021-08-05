77ceba21a439c232444f0a439b08e0f279aba0b8
mulifred <85351893+mulifred@users.noreply.github.com>
2021-08-05 12:38:26 +0300

    Add files via upload

---
+++ fredk.c
@@ -0,0 +1,11 @@
+//calling arrays by character
+#include <stdio.h>
+#include<strings.h>
+int main()
+{
+char FD[4]={'F','R','E','D','\0'};
+char F[]="CZAR";
+printf("the array value is:%s\n",FD);
+printf("the array value is:%s\n",F);	
+	return 0;
+}
